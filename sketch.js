b85723c1f479c41a72778e2c273007a74a3de869
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-22 16:10:41 -0300

    up

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let personagem;
 
 function preload(){
 imagemCenario=loadImage('imagens/cenario/floresta.png');
-imagemPersonagem=loadImage('imagens/personagem/megaman.png');
+imagemPersonagem=loadImage('imagens/personagem/correndo.png');
 somDoJogo=loadSound('sons/trilha_jogo.mp3');
 }
 
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario=new Cenario(imagemCenario,3);
   personagem=new Personagem(imagemPersonagem);
-  frameRate(10);
+  frameRate(40);
   somDoJogo.loop();
   
 }
